282624c7cb577423991bddbcbf03e1b273b6fa03
Mingyi Lu <mingyilu@stanford.edu>
2021-05-19 22:13:03 -0700

    Rename parser to parser_bw

--- test-mininet/run_1c.sh
+++ test-mininet/run_1c.sh
@@ -6,7 +6,7 @@ rm -rf outputs/1c*
 
 python fairness.py --dir=./outputs/1c --delay-bbr-others=0.5
 for f in $(ls outputs/1c/iperf3_server_*.log); do
-    python iperf3_log_parser.py < "$f" > "$f.raw"
+    python iperf3_log_parser_bw.py < "$f" > "$f.raw"
 done
 python plotter_1c.py
 
